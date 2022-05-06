0829fc13b3fa7846d1f4a974f04bd27b6204b43d
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2022-05-06 22:32:22 +0800

    feat(project): 缓存测试
    缓存测试

--- base/src/main/java/zlhywlf/base/Main.java
+++ base/src/main/java/zlhywlf/base/Main.java
@@ -1,14 +1,8 @@
 package zlhywlf.base;
 
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import zlhywlf.base.demo.IFacade;
-import zlhywlf.base.demo.model.CalData;
-import zlhywlf.base.demo.model.ResData;
-
-import javax.annotation.Resource;
 
 /**
  * @author zlhywlf
@@ -17,19 +11,16 @@ import javax.annotation.Resource;
 @ComponentScan("zlhywlf")
 public class Main {
 
-    @Resource(name="facade01")
-    IFacade<CalData, ResData> com;
 
 
+    public static void main(String[] args) throws InterruptedException {
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+//        Main bean = context.getBean(Main.class);
+//
+//        System.out.println(bean.cal.calculate("123"));
+//        context.close();
 
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
-        Main bean = context.getBean(Main.class);
-        CalData data = new CalData("test", 123.33);
-        System.out.println(bean.com.apply(data));
 
-
-        context.close();
 
     }
 
--- base/src/main/java/zlhywlf/base/demo/com/ComBean.java
+++
@@ -1,9 +0,0 @@
-package zlhywlf.base.demo.com;
-
-/**
- * @author zlhywlf
- */
-public interface ComBean {
-    String COM_01="composite01";
-    String COM_02="composite02";
-}
